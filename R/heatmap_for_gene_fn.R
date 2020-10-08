1c398137a4f60650b735eed66fc8f18af89cc68f
ainefairbrother <aine.fairbrother@hotmail.co.uk>
2020-10-08 17:03:39 +0100

    Fix gene conversion bug
    Sometimes duplicate gene codes were found by the converter function, so have added a test to see which ENS code is actually in the GTEx data and assign this to the 'gene' variable.

--- R/heatmap_for_gene_fn.R
+++ R/heatmap_for_gene_fn.R
@@ -16,95 +16,81 @@ heatmap_of_gene_corrs = function(gene, summary_brain, summary_controls){
     gene = convert_sym_ens(gene, input_ENS=F)
   }
   
-  if(gene %in% summary_brain$nuc_gene){
-    
-    ### preparing brain region data -----------------------------------------------------
-    
-    # filtering for gene R value and pivoting data
-    summary_gene_corrs = summary_brain %>% 
-      dplyr::filter(nuc_gene == gene) %>% 
-      dplyr::select(matches("corr|corrs|gene")) %>% 
-      tidyr::gather(key="region", value="corrs", -c("mt_gene", "nuc_gene", "gene_name")) %>% 
-      dplyr::arrange(region)
-    summary_gene_corrs$region = gsub("_corrs", "", summary_gene_corrs$region)
-    
-    # getting pvalues 
-    summary_gene_pval = summary_brain %>% 
-      dplyr::filter(nuc_gene == gene) %>% 
-      dplyr::select(matches("pval")) %>% 
-      tidyr::gather(key="region", value="pval") %>% 
-      dplyr::arrange(region)
-    summary_gene_pval$region = gsub("_pvals", "", summary_gene_pval$region)
-    
-    summary_gene = cbind(summary_gene_corrs %>% dplyr::select(-gene_name), pval=summary_gene_pval$pval)
-    
-    # df structure test
-    if(dim(summary_gene)[1] != length(unique(summary_gene$region))*13){
-      print(
-        paste("Incorrect row number for summary_gene. Table does not contain a per-mt gene pair for", gene, "for every tissue")
-      )
+  if(length(gene)>1){
+    for(g in gene){
+      if(g %in% summary_brain$nuc_gene){
+        gene=g
+      }
     }
-    
-    ### preparing control region data -----------------------------------------------------
-    if(gene %in% summary_controls$nuc_gene){
-      
-      # filtering for gene R value and pivoting data
-      ctrl_gene_corrs = summary_controls %>% 
-        dplyr::filter(nuc_gene == gene) %>% 
-        dplyr::select(matches("corr|gene")) %>% 
-        tidyr::gather(key="region", value="corrs", -c("mt_gene", "nuc_gene"))
-      ctrl_gene_corrs$region = gsub("_spearman_corrs", "", ctrl_gene_corrs$region)
-      
-      # getting pvalues 
-      ctrl_gene_pval = summary_controls %>% 
-        dplyr::filter(nuc_gene == gene) %>% 
-        dplyr::select(matches("pvals|gene")) %>% 
-        tidyr::gather(key="region", value="pval", -c("mt_gene", "nuc_gene"))
-      ctrl_gene_pval$region = gsub("_spearman_pvals", "", ctrl_gene_pval$region)
-      
-      summary_gene_ctrl = cbind(ctrl_gene_corrs, ctrl_gene_pval["pval"])
-      
-      print(head(summary_gene_ctrl))
-      print(head(summary_gene))
-      
-      summary_gene = rbind(summary_gene, summary_gene_ctrl)
-    }
-    
-    
-    # add sig indicator
-    summary_gene = summary_gene %>% mutate(sig_indicator = ifelse(summary_gene$pval < 0.05 & summary_gene$pval > 0.01, '*',
-                                                      ifelse(summary_gene$pval < 0.01 & summary_gene$pval > 0.001, '**',
-                                                             ifelse(summary_gene$pval < 0.001, '***', ''))))
-    
-    # tidying region names
-    summary_gene$region = gsub("Brain", "", 
-                               gsub("_corrs", "", 
-                                    gsub("basalganglia", "BG", 
-                                         gsub("_spearman", "", 
-                                              gsub("muscle", "Muscle", 
-                                                   gsub("heart", "Heart", summary_gene$region))))))
-    
-    # # converting mt_gene genes
-    summary_gene$mt_gene = convert_sym_ens(summary_gene$mt_gene, same_length=TRUE)
-
-    #plotting heatmap
-    p = ggplot(summary_gene, aes(x=reorder(region, corrs), y=reorder(mt_gene, corrs), fill=corrs)) +
-             theme_classic(base_size=11) +
-             theme(plot.title = element_text(hjust = 0.5)) +
-             geom_tile() +
-             theme(axis.text.x = element_text(angle = 45, hjust = 1)) + # rotates x axis
-             ggtitle(gene_sym) +
-             theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0, size=9)) +
-             scale_fill_gradient2(low = "red", mid = "white", high = "blue", limits=c(-1,1)) +
-             geom_text(aes(label=sig_indicator), size=3) +
-             labs(y="mtDNA-encoded gene", x="", fill=expression(rho), subtitle = "* 0.01<p<0.05, ** 0.001<p<0.01, *** p<0.001")
-    
-    return(p)
-    
-  } else {
-    return("")
   }
+  
+  ### preparing brain region data -----------------------------------------------------
+  
+  # filtering for gene R value and pivoting data
+  summary_gene_corrs = summary_brain %>% 
+    dplyr::filter(nuc_gene == gene) %>% 
+    dplyr::select(matches("corr|gene")) %>% 
+    tidyr::gather(key="region", value="corrs", -c("mt_gene", "nuc_gene", "gene_name")) %>% 
+    dplyr::arrange(region)
+  
+  # getting pvalues 
+  summary_gene_pval = summary_brain %>% 
+    dplyr::filter(nuc_gene == gene) %>% 
+    dplyr::select(matches("pval")) %>% 
+    tidyr::gather(key="region", value="pval") %>% 
+    dplyr::arrange(region)
+  
+  summary_gene = cbind(summary_gene_corrs %>% dplyr::select(-gene_name), pval=summary_gene_pval$pval)
+  
+  ### preparing control region data -----------------------------------------------------
+  
+  # filtering for gene R value and pivoting data
+  ctrl_gene_corrs = summary_controls %>% 
+    dplyr::filter(nuc_gene == gene) %>% 
+    dplyr::select(matches("corr|gene")) %>% 
+    tidyr::gather(key="region", value="corrs", -c("mt_gene", "nuc_gene"))
+  
+  # getting pvalues 
+  ctrl_gene_pval = summary_controls %>% 
+    dplyr::filter(nuc_gene == gene) %>% 
+    dplyr::select(matches("pvals|gene")) %>% 
+    tidyr::gather(key="region", value="pval", -c("mt_gene", "nuc_gene"))
+  
+  summary_gene_ctrl = cbind(ctrl_gene_corrs, ctrl_gene_pval["pval"])
+  
+  summary_gene = rbind(summary_gene, summary_gene_ctrl)
+  
+  # add sig indicator
+  summary_gene = summary_gene %>% mutate(sig_indicator = ifelse(summary_gene$pval < 0.05 & summary_gene$pval > 0.01, '*',
+                                                                ifelse(summary_gene$pval < 0.01 & summary_gene$pval > 0.001, '**',
+                                                                       ifelse(summary_gene$pval < 0.001, '***', ''))))
+  
+  # tidying region names
+  summary_gene$region = gsub("Brain", "", 
+                             gsub("_corrs", "", 
+                                  gsub("basalganglia", "BG", 
+                                       gsub("_spearman", "", 
+                                            gsub("muscle", "Muscle", 
+                                                 gsub("heart", "Heart", summary_gene$region))))))
+  
+  # # converting mt_gene genes
+  summary_gene$mt_gene = convert_sym_ens(summary_gene$mt_gene, same_length=TRUE)
+  
+  #plotting heatmap
+  p = ggplot(summary_gene, aes(x=reorder(region, corrs), y=reorder(mt_gene, corrs), fill=corrs)) +
+    theme_classic(base_size=11) +
+    theme(plot.title = element_text(hjust = 0.5)) +
+    geom_tile() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) + # rotates x axis
+    ggtitle(gene_sym) +
+    theme(plot.title = element_text(hjust = 0.5), plot.subtitle = element_text(hjust = 0, size=9)) +
+    scale_fill_gradient2(low = "red", mid = "white", high = "blue", limits=c(-1,1)) +
+    geom_text(aes(label=sig_indicator), size=3) +
+    labs(y="mtDNA-encoded gene", x="", fill=expression(rho), subtitle = "* 0.01<p<0.05, ** 0.001<p<0.01, *** p<0.001")
+  
+  return(p)
+  
 }
 
 #test
-#heatmap_of_gene_corrs(gene="PINK1", summary_brain=summary_brain, summary_controls=summary_controls) #ENSG00000005194
+heatmap_of_gene_corrs(gene="SOD2", summary_brain=summary_brain, summary_controls=summary_controls) #ENSG00000005194
