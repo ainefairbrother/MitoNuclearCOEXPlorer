61657c6b0aeb1c2dd855b67c74a02dfd028c6ee5
ainefairbrother <aine.fairbrother@hotmail.co.uk>
2020-11-14 11:54:36 +0000

    Add catchall for case with multi-mapping gene syms

--- R/gen_gene_specific_distr_plot_fn.R
+++ R/gen_gene_specific_distr_plot_fn.R
@@ -14,22 +14,35 @@ genDistributionPlotWithGene = function(gene, summary_brain){
     gene_sym = gene
     gene = convert_sym_ens(gene, input_ENS=F, load_genespace=F)
   }
-
+  
+  # for when gene sym maps to two ENS codes  
+  if(length(gene) > 1){
+    for(g in gene){
+      if(length(rownames(summary_brain %>% dplyr::filter(nuc_gene == g))) > 0){
+        gene = g
+      }
+    }
+  }
+  
+  print(gene)
+  print(gene_sym)
+  
   # filtering for gene R value and pivoting data
   summary_brain_clean = summary_brain %>% 
     dplyr::select(matches("corr|nuc_gene")) %>% 
     tidyr::pivot_longer(2:13, names_to='region', values_to='R_value') %>% 
     dplyr::mutate(region = gsub('_corrs', '', gsub('Brain', '', gsub('basalganglia', 'BG', region))))
-    
+  
   gene_mean_across_mt_df = summary_brain_clean %>%
     dplyr::filter(nuc_gene == gene) %>%
     dplyr::group_by(region) %>%
     dplyr::mutate(gene_mean_across_mt = round(mean(R_value), 4))
-  
+
   gene_mean_label_df = data.frame(
     region = unique(gene_mean_across_mt_df$region),
-    label = unique(gene_mean_across_mt_df$gene_mean_across_mt))
-
+    label = unique(gene_mean_across_mt_df$gene_mean_across_mt)) %>% 
+    dplyr::arrange(label)
+  
   distPlot = ggplot(data=summary_brain_clean, aes(x=R_value)) +
     theme_minimal(base_size=11) +
     theme(plot.title = element_text(hjust = 0.5)) +
@@ -39,16 +52,16 @@ genDistributionPlotWithGene = function(gene, summary_brain){
     xlim(-1,1) +
     xlab(expression(rho)) +
     ylab('Density') +
-
+    
     # add line to indicate mean rho of gene across 13 mt genes
     geom_vline(
       data = gene_mean_label_df,
       mapping = aes(
-      xintercept = label,
-      colour='mean correlation value across 13 mtDNA genes'),
+        xintercept = label,
+        colour='mean correlation value across 13 mtDNA genes'),
       linetype='dashed',
       size=0.5) +
-
+    
     # add annotation to distplot: mean rho
     geom_label(
       size=3,
@@ -60,18 +73,19 @@ genDistributionPlotWithGene = function(gene, summary_brain){
       colour = 'white',
       fontface = 'bold'
     )  +
-
+    
     # add legend
     scale_color_manual(name = '', values = c('mean correlation value across 13 mtDNA genes'= '#3016FF'),
                        labels = paste(gene_sym, 'mean correlation value across 13 mtDNA genes'))
-
+  
   return(distPlot)
   
 }
 
 # #test
 # start.time = Sys.time()
-# genDistributionPlotWithGene(gene='ATG7', summary_brain=summary_brain)
+# gene='SOD2'
+# genDistributionPlotWithGene(gene=gene, summary_brain=summary_brain)
 # end.time = Sys.time()
 # time.taken = end.time - start.time
 # print(time.taken)
