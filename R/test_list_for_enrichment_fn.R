0743245a8d5be2a01cd28fdbe6a854ca74f1f8bf
ainefairbrother <aine.fairbrother@hotmail.co.uk>
2020-11-14 13:56:27 +0000

    RAM usage optimisations

--- R/test_list_for_enrichment_fn.R
+++ R/test_list_for_enrichment_fn.R
@@ -233,11 +233,11 @@ test_list_for_enrichment = function(gene_list, iters, filt_bkg_for_protein_codin
   
 }
 
-start.time = Sys.time()
-ls = "ALKBH1, C1QBP, CDK5RAP1"
-test_list_for_enrichment(ls, iters=1000, T)
-end.time = Sys.time()
-time.taken = end.time - start.time
-print(time.taken)
+# start.time = Sys.time()
+# ls = "ALKBH1, C1QBP, CDK5RAP1"
+# test_list_for_enrichment(ls, iters=1000, T)
+# end.time = Sys.time()
+# time.taken = end.time - start.time
+# print(time.taken)
 
 
